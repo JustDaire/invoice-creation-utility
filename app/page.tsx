5751f1888eec20e53d84e19cf9668f5cc6cca151
Daire <daire@protonmail.com>
2024-12-09 13:38:55 +0000

    Added Company and Client form fields

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@nextui-org/button";
+import { Input, Textarea } from "@nextui-org/input";
 import React, { useState } from "react";
 
 type Column = {
@@ -84,91 +85,209 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       {/* Main grid */}
-      <div>
-        {/*  onSubmit={handleSubmit} */}
-        <form className="grid gap-4">
-          <div className="grid grid-cols-2 gap-4 flex items-start">
-            {/* Issuer details */}
-            <div className="grid gap-4">
-              <ul>
-                <li>Company Name</li>
-                <li>Company Email</li>
-                <li>Company Phone</li>
-                <li>Company Address</li>
-                <li>Company VAT number</li>
-              </ul>
-            </div>
-            {/* Client details */}
-            <div className="grid gap-4">
-              <ul>
-                <li>Client Name</li>
-                <li>Client Email</li>
-                <li>Client Phone</li>
-                <li>Client Address</li>
-              </ul>
-            </div>
+      {/*  onSubmit={handleSubmit} */}
+      <form className="grid gap-4">
+        <div className="grid grid-cols-2 gap-4 flex items-start">
+          {/* Issuer details */}
+          <div className="grid gap-4">
+            <Input
+              aria-label="Company Name"
+              label="Name"
+              type="text"
+              value={formData.company.name}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  company: {
+                    ...formData.company,
+                    name: e.target.value,
+                  },
+                })
+              }
+            />
+            <Input
+              aria-label="Company Email"
+              label="Email"
+              type="email"
+              value={formData.company.email}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  company: {
+                    ...formData.company,
+                    email: e.target.value,
+                  },
+                })
+              }
+            />
+            <Input
+              aria-label="Company Phone"
+              label="Phone"
+              type="text"
+              value={formData.company.phone}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  company: {
+                    ...formData.company,
+                    phone: e.target.value,
+                  },
+                })
+              }
+            />
+            <Textarea
+              aria-label="Company address"
+              label="Address"
+              value={formData.company.address}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  company: {
+                    ...formData.company,
+                    address: e.target.value,
+                  },
+                })
+              }
+            />
+            <Input
+              aria-label="Company VAT Number"
+              label="VAT Number"
+              type="text"
+              value={formData.company.vat_number}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  company: {
+                    ...formData.company,
+                    vat_number: e.target.value,
+                  },
+                })
+              }
+            />
           </div>
-          <div className="grid grid-cols-3 gap-4">
-            {/* Invoice Number */}
-            <div className="grid gap-4">Invoice Number</div>
-            {/* Invoice Date Picker */}
-            <div className="grid gap-4">Invoice Date</div>
-            {/* Terms Selector */}
-            <div className="grid gap-4">
-              Terms
-              <br />
-              Due Date
-            </div>
+          {/* Client details */}
+          <div className="grid gap-4">
+            <Input
+              aria-label="Client Name"
+              label="Name"
+              type="text"
+              value={formData.client.name}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  client: {
+                    ...formData.client,
+                    name: e.target.value,
+                  },
+                })
+              }
+            />
+            <Input
+              aria-label="Client Email"
+              label="Email"
+              type="email"
+              value={formData.client.email}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  client: {
+                    ...formData.client,
+                    email: e.target.value,
+                  },
+                })
+              }
+            />
+            <Input
+              aria-label="Client Phone"
+              label="Phone"
+              type="text"
+              value={formData.client.phone}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  client: {
+                    ...formData.client,
+                    phone: e.target.value,
+                  },
+                })
+              }
+            />
+            <Textarea
+              aria-label="Client address"
+              label="Address"
+              value={formData.client.address}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  client: {
+                    ...formData.client,
+                    address: e.target.value,
+                  },
+                })
+              }
+            />
           </div>
-          {/* Invoice items */}
-          <div className="grid grid-cols-1 gap-4">
-            <div className="grid gap-4">
-              <table>
-                <thead>
-                  <tr>
-                    <th>Action</th>
-                    <th>Description</th>
-                    <th>Rate</th>
-                    <th>Quantity</th>
-                    <th>Total</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      <button>x</button>
-                    </td>
-                    <td>Item 1</td>
-                    <td>1</td>
-                    <td>100</td>
-                    <td>100</td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <button>x</button>
-                    </td>
-                    <td>Item 2</td>
-                    <td>1</td>
-                    <td>200</td>
-                    <td>200</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+        </div>
+        <div className="grid grid-cols-3 gap-4">
+          {/* Invoice Number */}
+          <div className="grid gap-4">Invoice Number</div>
+          {/* Invoice Date Picker */}
+          <div className="grid gap-4">Invoice Date</div>
+          {/* Terms Selector */}
+          <div className="grid gap-4">
+            Terms
+            <br />
+            Due Date
           </div>
-          {/* Invoice notes */}
-          <div className="grid grid-cols-1 gap-4">
-            <div className="grid gap-4">Notes</div>
+        </div>
+        {/* Invoice items */}
+        <div className="grid grid-cols-1 gap-4">
+          <div className="grid gap-4">
+            <table>
+              <thead>
+                <tr>
+                  <th>Action</th>
+                  <th>Description</th>
+                  <th>Rate</th>
+                  <th>Quantity</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <button>x</button>
+                  </td>
+                  <td>Item 1</td>
+                  <td>1</td>
+                  <td>100</td>
+                  <td>100</td>
+                </tr>
+                <tr>
+                  <td>
+                    <button>x</button>
+                  </td>
+                  <td>Item 2</td>
+                  <td>1</td>
+                  <td>200</td>
+                  <td>200</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-          {/* Invoice actions */}
-          <div className="grid grid-cols-1 gap-4">
-            <div className="grid gap-4">
-              <button type="submit">Generate</button>
-              <button>Reset</button>
-            </div>
+        </div>
+        {/* Invoice notes */}
+        <div className="grid grid-cols-1 gap-4">
+          <div className="grid gap-4">Notes</div>
+        </div>
+        {/* Invoice actions */}
+        <div className="grid grid-cols-1 gap-4">
+          <div className="grid gap-4">
+            <button type="submit">Generate</button>
+            <button>Reset</button>
           </div>
-        </form>
-      </div>
+        </div>
+      </form>
     </section>
   );
 }
