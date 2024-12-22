f31fc8dae32999f355fd7665a4be24931b47d969
Daire <daire@protonmail.com>
2024-12-22 22:56:10 +0000

    Moved "termPeriods" array to seperate file

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ import { Divider } from "@nextui-org/divider";
 import { Select, SelectItem } from "@nextui-org/select";
 import { DatePicker } from "@nextui-org/date-picker";
 import { MdAdd, MdDelete } from "react-icons/md";
+import { termPeriods } from "./data";
 
 type Column = {
   key: string;
@@ -70,69 +71,6 @@ const columns = [
     key: "amount",
     label: "AMOUNT",
     type: "number",
-  },
-];
-
-const termPeriods = [
-  {
-    key: "none",
-    label: "None",
-    value: null,
-  },
-  {
-    key: "7days",
-    label: "7 days",
-    value: 7,
-  },
-  {
-    key: "10days",
-    label: "10 days",
-    value: 10,
-  },
-  {
-    key: "14days",
-    label: "14 days",
-    value: 14,
-  },
-  {
-    key: "21days",
-    label: "21 days",
-    value: 21,
-  },
-  {
-    key: "30days",
-    label: "30 days",
-    value: 30,
-  },
-  {
-    key: "45days",
-    label: "45 days",
-    value: 45,
-  },
-  {
-    key: "60days",
-    label: "60 days",
-    value: 60,
-  },
-  {
-    key: "90days",
-    label: "90 days",
-    value: 90,
-  },
-  {
-    key: "120days",
-    label: "120 days",
-    value: 120,
-  },
-  {
-    key: "180days",
-    label: "180 days",
-    value: 180,
-  },
-  {
-    key: "365days",
-    label: "365 days",
-    value: 365,
   },
 ];
 
